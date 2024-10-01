2ddae867cd47614923102f71d76ca4eaf7ba3580
София Богданова <bsd@pkzdrav.ru>
2024-10-01 22:40:01 +0300

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Menu />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from './components/layout/Footer.vue'
+import Menu from './components/layout/Menu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Footer,
+    Menu
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './assets/reset.css'
+import './assets/main.css'
+import App from './App.vue'
+import router from './router'
+
+createApp(App).mount('#app')
