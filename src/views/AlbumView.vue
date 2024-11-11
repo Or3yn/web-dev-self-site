ad0f885ca11942599babf36f80e925f3bf8fa711
Or3yn <zxcbogdanova@gmail.com>
2024-11-11 10:27:27 +0300

    lightbox drraft

--- src/views/AlbumView.vue
+++ src/views/AlbumView.vue
@@ -8,6 +8,7 @@
         class="album-view__item"
         @mouseover="hover = index"
         @mouseleave="hover = null"
+        @click="openLightbox(index)"
         :class="{ 'hovered': hover === index }"
       >
         <img :src="photo.imageSrc" class="album-view__image" />
@@ -16,8 +17,35 @@
         </div>
       </div>
     </div>
+
+    <!-- Lightbox Overlay -->
+    <div v-if="lightboxActive" class="lightbox">
+      <div class="lightbox__content">
+        <img :src="photos[currentIndex].imageSrc" class="lightbox__image" />
+        <p class="lightbox__caption">{{ photos[currentIndex].title }}</p>
+        <img
+          src="../assets/icons/clear.png"
+          class="lightbox__close"
+          @click="closeLightbox"
+          alt="Close"
+        />
+        <img
+          src="../assets/icons/arrow.png"
+          class="lightbox__arrow lightbox__arrow--right"
+          @click="nextPhoto"
+          alt="Next"
+        />
+        <img
+          src="../assets/icons/popcorn.png"
+          class="lightbox__arrow lightbox__arrow--left"
+          @click="prevPhoto"
+          alt="Previous"
+        />
+      </div>
+    </div>
   </div>
 </template>
+
 
 <script>
 import { albumPhotos } from '../const/AlbumConst.js'
@@ -27,7 +55,25 @@ export default {
     return {
       hover: null,
       photos: albumPhotos,
+      lightboxActive: false,
+      currentIndex: 0,
     };
+  },
+  methods: {
+    openLightbox(index) {
+      this.currentIndex = index;
+      this.lightboxActive = true;
+    },
+    closeLightbox() {
+      this.lightboxActive = false;
+    },
+    nextPhoto() {
+      this.currentIndex = (this.currentIndex + 1) % this.photos.length;
+    },
+    prevPhoto() {
+      this.currentIndex =
+        (this.currentIndex - 1 + this.photos.length) % this.photos.length;
+    },
   },
 };
 </script>
@@ -82,7 +128,7 @@ export default {
 
 .album-view__item:hover .album-view__image {
   transform: scale(1.05);
-  filter: grayscale(50%); /* Полу-затемнение */
+  filter: grayscale(50%);
 }
 
 .album-view__item:hover .album-view__overlay {
@@ -109,5 +155,65 @@ export default {
 
 .album-view__item:hover::before {
   opacity: 1;
+}
+
+/* Lightbox styles */
+.lightbox {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.lightbox__content {
+  position: relative;
+  width: 60%;
+  max-width: 600px;
+  text-align: center;
+}
+
+.lightbox__image {
+  width: 100%;
+  border-radius: 10px;
+}
+
+.lightbox__caption {
+  color: #fff;
+  margin-top: 10px;
+  font-size: 1.2em;
+}
+
+.lightbox__close {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 50px; /* Increased size */
+  height: 50px;
+  cursor: pointer;
+  z-index: 1100; /* Ensure it’s above other elements */
+  overflow: visible;
+}
+
+
+.lightbox__arrow {
+  position: absolute;
+  top: 50%;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+}
+
+.lightbox__arrow--right {
+  right: 20px;
+}
+
+.lightbox__arrow--left {
+  left: 20px;
 }
 </style>
